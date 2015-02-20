dd4b60e87b9266810d636596c2b739b66635ccec
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-20 13:08:46 -0700

    added some validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,13 @@
 class Person < ActiveRecord::Base
 
+  validate :person_has_first_name
+  validates :last_name, presence: {message: "can't be blank, your ancestors would be pissed!"}
+  validates :date_of_birth, presence: {message: "can't be blank, I'd bet your life you have a birthday!"}
 
+  def person_has_first_name
+    unless self.first_name
+      self.errors[:base] << "Better not be fucking with me, fool!"
+    end
+  end
 
 end

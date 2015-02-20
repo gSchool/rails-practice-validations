adcd084764fe4d2dcdddb0b2d2d9c40076f6902c
Charles Painter <cpainter85@gmail.com>
2015-02-20 15:43:49 -0700

    Removed commented out test

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -62,14 +62,3 @@ feature 'People' do
     expect(page).to have_content('Date of birth can\'t be in the future')
   end
 end
-
-#
-# feature 'Organizations' do
-#
-#   scenario 'Organizations must have a Name' do
-#     visit organizations_path
-#     click_on 'New Organization'
-#     click_on 'Create Organization'
-#     expect(page).to have_no_content("saved successfully")
-#     expect(page).to have_content('Name can\'t be blank')
-#   end

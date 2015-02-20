64f5d66daf23ce1f530dbeb680761ede5cb34d6b
Melani Dizon <dizonmelani@gmail.com>
2015-02-20 12:48:26 -0700

    added validations, error messages, and partials

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,9 +10,13 @@ class OrganizationsController < ApplicationController
 
   def create
     @organization = Organization.new(organization_params)
-    @organization.save
+    if @organization.save
     redirect_to organizations_path, notice: "Organization saved successfully"
+  else
+    render :new
   end
+end
+
 
   def edit
     @organization = Organization.find(params[:id])

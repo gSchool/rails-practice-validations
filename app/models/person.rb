aaebc7f604ef08066f74ee1df7119902fc1c6ec1
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-25 09:00:31 -0700

    added partials

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,12 +3,5 @@ class Person < ActiveRecord::Base
   validates :first_name, presence: {message: "can't be blank, didn't your mother give you a name?" }
   validates :last_name, presence: {message: "can't be blank, your ancestors would be pissed!"}
   validates :date_of_birth, presence: {message: "can't be blank, I'd bet your life you have a birthday!"}
-  validate :person_has_first_name
-
-  def person_has_first_name
-    unless self.first_name == "Denzel Washington"
-      self.errors[:base] << "Better not be fucking with me, fool!"
-    end
-  end
 
 end

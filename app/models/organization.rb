d278f5c41b7524562749e4c187feb4790414de99
Melani Dizon <dizonmelani@gmail.com>
2015-02-25 11:25:38 -0700

    added organization spec file and tested name, uniqueness, and sorted by letter with to_include

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,10 @@
 class Organization < ActiveRecord::Base
 
   validates :name, presence: true
+  validates :name, uniqueness: true
+
+  def self.by_letter(letter)
+   where("name LIKE ?", "#{letter}%").order(:name)
+ end
 
 end
